ba092b2aaa93b273f2c464e912757cb8e7415ebc
Elboudali Mohamed <mohamedelboudali123@gmail.com>
2019-05-10 08:29:39 +0200

    update R_peaks

--- src/R_peaks.m
+++ src/R_peaks.m
@@ -1,8 +1,9 @@
-function [R_pks, R_locs] = R_peaks(data, smw)
+function [R_pks, R_locs] = R_peaks(data, smw,Fs)
+m=abs(mean(smw.*data));
 for i=1:length(data)
-    if (smw(i)*data(i)==0)
+    if (smw(i)*data(i)<m)
         data(i)=0;
     end
 end
-[R_pks,R_locs] = findpeaks(data,'MinPeakDistance',50);
+[R_pks,R_locs] = findpeaks(data,'MinPeakDistance',round(0.2*Fs));
 end
